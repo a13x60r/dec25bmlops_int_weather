6e1fca81b3f1cedbaa9c7fc5909817f6606fe6c2
a13x60r <a13x60r>
2026-01-17 11:39:19 +0100

    fix: secure API key using environment variables

--- dags/data_update_dag.py
+++ dags/data_update_dag.py
@@ -1,9 +1,10 @@
-from datetime import datetime
-from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
-from airflow.datasets import Dataset
-from docker.types import Mount
 import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.datasets import Dataset
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
 
 weather_dataset = Dataset("file:///data/raw/weatherAUS.csv")
 
@@ -12,14 +13,13 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     schedule="@daily",
     catchup=False,
-    tags=['mlops', 'data_eng'],
+    tags=["mlops", "data_eng"],
 ) as dag:
-
     # Run the fetch_weather_data.py script inside the container
     fetch_new_data = DockerOperator(
         task_id="fetch_weather_api",
-        image='dec25bmlops_int_weather-trainer:latest',
-        api_version='auto',
+        image="dec25bmlops_int_weather-trainer:latest",
+        api_version="auto",
         auto_remove=True,
         # Re-installing requests just in case (though we added it to requirements, image rebuild might be pending in some contexts)
         # Actually we rebuilt the image in step 339, so requests should be there.
@@ -27,11 +27,15 @@ with DAG(
         docker_url="unix://var/run/docker.sock",
         network_mode="dec25bmlops_int_weather_default",
         mounts=[
-            Mount(source='C:/Users/aboro/Documents/dataScientist/dec25bmlops_int_weather', target='/workspace', type='bind'),
+            Mount(
+                source="C:/Users/aboro/Documents/dataScientist/dec25bmlops_int_weather",
+                target="/workspace",
+                type="bind",
+            ),
         ],
         environment={
-            'OPENWEATHER_API_KEY': os.getenv("OPENWEATHER_API_KEY", "2382ead9008d54d34ff156b1bbe44f6d"),
-            'MLFLOW_TRACKING_URI': 'http://mlflow:5000',
+            "OPENWEATHER_API_KEY": os.getenv("OPENWEATHER_API_KEY"),
+            "MLFLOW_TRACKING_URI": "http://mlflow:5000",
         },
-        outlets=[weather_dataset]
+        outlets=[weather_dataset],
     )
